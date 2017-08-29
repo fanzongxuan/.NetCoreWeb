f6255c362bd1ba05cd518dbca6bd3f143224d8e3
fanzongxuan@perfay.com <fanzongxuan@perfay.com>
2017-08-29 17:36:39 +0800

    DistributeCache(rediscache)

---
+++ src/DotNetCore.Core/Cache/DistributeCacheManager.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore.Core.Cache
+{
+    public class DistributeCacheManager : ICacheManager
+    {
+        private readonly IDistributedCache _distributedCache;
+        private readonly ICacheManager _perRequestCacheManager;
+
+        public DistributeCacheManager(IDistributedCache distributedCache,
+            ICacheManager perRequestCacheManager)
+        {
+            _distributedCache = distributedCache;
+            _perRequestCacheManager = perRequestCacheManager;
+        }
+
+        protected virtual string Serialize(object item)
+        {
+            var jsonString = JsonConvert.SerializeObject(item);
+            return jsonString;
+        }
+        protected virtual T Deserialize<T>(string jsonString)
+        {
+            if (jsonString == null)
+                return default(T);
+
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        public void Clear()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public T Get<T>(string key)
+        {
+            //this way we won't connect to Redis server 500 times per HTTP request (e.g. each time to load a locale or setting)
+            if (_perRequestCacheManager.IsSet(key))
+                return _perRequestCacheManager.Get<T>(key);
+
+            var rValue = _distributedCache.GetString(key);
+            if (string.IsNullOrEmpty(rValue))
+                return default(T);
+            var result = Deserialize<T>(rValue);
+
+            _perRequestCacheManager.Set(key, result, 0);
+            return result;
+        }
+
+        public bool IsSet(string key)
+        {
+            if (_perRequestCacheManager.IsSet(key))
+                return true;
+
+            return !string.IsNullOrWhiteSpace(_distributedCache.GetString(key));
+        }
+
+        public void Remove(string key)
+        {
+            _distributedCache.Remove(key);
+            _perRequestCacheManager.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+                //foreach (var key in keys)
+                //    Remove(key);
+            
+        }
+
+        public void Set(string key, object data, int cacheTime)
+        {
+            if (data == null)
+                return;
+
+            var entryBytes = Serialize(data);
+            var expiresIn = TimeSpan.FromMinutes(cacheTime);
+            DistributedCacheEntryOptions opt = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = expiresIn
+            };
+            _distributedCache.SetString(key, entryBytes, opt);
+        }
+    }
+}
--- src/DotNetCore.Core/Infrastructure/DependencyManagement/Dependency.cs
+++ src/DotNetCore.Core/Infrastructure/DependencyManagement/Dependency.cs
@@ -4,14 +4,29 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using DotNetCore.Core.Cache;
+using Microsoft.Extensions.Configuration;
+using Autofac;
+using Autofac.Core;
 
 namespace DotNetCore.Core.Infrastructure.DependencyManagement
 {
     public class Dependency : IDependency
     {
-        public void Register(IServiceCollection services)
+        public int Order => 0;
+
+        public void Register(ContainerBuilder builder, AppDomainTypeFinder typeFinder, IConfiguration configuration)
         {
-            services.AddScoped<ICacheManager, MemoryCacheManager>();
+            builder.RegisterType<PerRequestCacheManager>().As<ICacheManager>().Named<ICacheManager>("cache_per_request");
+
+            var redisEnable = configuration.GetValue<bool>("Redis:Enable");
+            if (redisEnable)
+            {
+                builder.RegisterType<DistributeCacheManager>().As<ICacheManager>().Named<ICacheManager>("cache_static").WithParameter(ResolvedParameter.ForNamed<ICacheManager>("cache_per_request"));
+            }
+            else
+            {
+                builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().Named<ICacheManager>("cache_static");
+            }
         }
     }
 }
