9ae629a4527575b3af6e2b7611fc8438b6d73821
fanzongxuan <1027300882@qq.com>
2017-09-17 11:37:15 +0800

    CustomerLastActivityAttribute

---
+++ src/DotNetCore.Framework/Attributes/CustomerLastActivityAttribute.cs
@@ -0,0 +1,36 @@
+﻿using DotNetCore.Core.Infrastructure;
+using DotNetCore.Core.Interface;
+using DotNetCore.Service.Accounts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore.Framework.Attributes
+{
+    public class CustomerLastActivityAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context == null || context.HttpContext == null || context.HttpContext.Request == null)
+                return;
+
+            //only GET requests
+            if (!String.Equals(context.HttpContext.Request.Method, "GET", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            var workContext = EngineContext.Current.GetService<IWorkContext>();
+            var customer = workContext.CurrentAccount;
+
+            //update last activity date
+            if (customer.LastActivityDateUtc.AddMinutes(1.0) < DateTime.UtcNow)
+            {
+                var customerService = EngineContext.Current.GetService<IAccountService>();
+                customer.LastActivityDateUtc = DateTime.UtcNow;
+                customerService.Update(customer);
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
